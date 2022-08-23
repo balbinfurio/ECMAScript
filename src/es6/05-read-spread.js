bab1e12f484f5ad169460d2ab06b6d8521ab2f64
Balbin <xxbalbin@gmail.com>
2022-08-23 17:02:04 -0500

    rest params

--- src/es6/05-read-spread.js
+++ src/es6/05-read-spread.js
@@ -16,3 +16,31 @@ let { username, age} = user; // First the values that I want to get, then the va
 console.log(username, age); // Balbin 21
 
 console.log(user.username, user.age); // Balbin 21
+
+
+// spread operator
+
+let person = {
+    name: 'furio',
+    age: 25
+};
+
+let country = 'greece';
+
+let data = { ...person, country};
+console.log(data); // { name: 'furio', age: 25, country: 'greece' }
+data = {id: 1037, ...person, country}; // You could add values at the beginning as well
+console.log(data); // { id: 1037, name: 'furio', age: 25, country: 'greece' }
+
+// rest params
+
+const sum = (num, ...values) =>
+{
+    console.log(values); // [ 1, 2, 3 ]
+    console.log(num + values[0]); // 2
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3);
+
+

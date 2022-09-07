afb890af11ead8c4fc3f159a7b29cd3044535fc7
balbinfurio <xxbalbin@email.com>
2022-09-07 16:14:17 -0500

    updates 7

--- src/es6/06-object-literals.js
+++ src/es6/06-object-literals.js
@@ -1,6 +1,6 @@
 // enhanced object literals
 
-const newUser = (user, age, country) =>
+const newUser = (user, age, country) => // old way
 {
     return{
         user: user,
@@ -9,7 +9,8 @@ const newUser = (user, age, country) =>
     }
 }
 
-const properWay = (user, age, country) =>
+const properWay = (user, age, country) => // new best and proper way
+0
 {
     return{
         user,
@@ -18,5 +19,5 @@ const properWay = (user, age, country) =>
     }
 }
 
-console.log(newUser('Furio', 25, 'Transylvania'));
-console.log(properWay('Furio', 50, 'Transylvania')); 
+console.log(newUser('Furio', 25, 'Transylvania')); // { user: 'Furio', age: 25, country: 'Transylvania' }
+console.log(properWay('Furio', 50, 'Transylvania')); // { user: 'Furio', age: 25, country: 'Transylvania' }

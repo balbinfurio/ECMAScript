ec6e9879f537f82a0b7847405f8f3a2665480a61
balbinfurio <xxbalbin@email.com>
2022-09-14 10:56:43 -0500

    updates

---
+++ src/es10/01-trimstart-trime.js
@@ -0,0 +1,6 @@
+const hello = '    Hello World!    ';
+
+// remove spaces of the string
+
+console.log(hello.trimStart());
+console.log(hello.trimEnd());
---
+++ src/es9/02-finally.js
@@ -0,0 +1,14 @@
+const anotherFunction = () => {
+  return new Promise((resolve, reject) => {
+    if (false) {
+      resolve('Works!!'); // response(below)
+    } else {
+      reject('fuck!'); // err(below)
+    };
+  });
+};
+
+anotherFunction()
+  .then(response => console.log(response)) //  response or any word you want
+  .catch(err => console.log(err))
+  .finally(() => console.log('Finally'))
